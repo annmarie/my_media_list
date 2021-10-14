3f8217301ddf3fcca270a19ac0e6c60d1b7bb23e
AnnMarie Conway <aconway2@meredith.com>
2021-10-13 21:49:06 -0400

    create table for home page display

--- components/home-component.js
+++ components/home-component.js
@@ -1,51 +1,100 @@
 import _ from 'lodash';
-import { useState, useEffect } from 'react';
+import styles from 'styles/components/Home.module.scss';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
 export default function HomeComponent(props) {
-  const [data, setData] = useState(0);
+  const queryClient = new QueryClient();
 
-  useEffect(() => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <SubscriptionList {...props} />
+      </QueryClientProvider>
+    </>
+  );
+}
+
+function SubscriptionList(props) {
+  const [stateData, setData] = useState(0);
+
+  const { status, data } = useQuery('subscriptions', () => {
+    return new Promise((good, bad) => {
+      const storedData = localStorage.getItem(props.localStorageKey);
+      if (storedData) {
+        try {
+          const storedJson = JSON.parse(storedData);
+          if (_.get(storedJson, 'payload')) {
+            good(storedJson);
+          } else {
+            // invalid local storage found return empty data set
+            good({});
+          }
+        } catch (e) {
+          bad(e);
+        }
+      }
+      // no local storage found return empty data set
+      good({});
+    }).then((data) => data);
+  });
+
+  if (status === 'loading') return <div>loading...</div>;
+  else if (status === 'success' && !stateData) setData(data);
+
+  return _.get(stateData, 'payload') ? <TableData {...stateData} /> : <InitButton {...props} setData={setData} />;
+}
+
+function InitButton(props) {
+  // initialize demo data
+  const initData = async () => {
     const key = props.localStorageKey;
-    const storedData = localStorage.getItem(key);
-    if (storedData)
-      try {
-        const storedJson = JSON.parse(storedData);
-        if (_.get(storedJson, 'payload')) setData(JSON.parse(storedData));
-      } catch (e) {
-        // catch error
-      }
-  }, [props, setData]);
-
-  if (!data) {
-    // initialize demo data
-    const initData = async () => {
-      const key = props.localStorageKey;
-      let demoData = {};
-      try {
-        const rset = await fetch('/api/data');
-        demoData = await rset.json();
-      } catch (err) {
-        console.log('data not found');
-        console.log(err);
-      }
-      localStorage.setItem(key, JSON.stringify(demoData));
-      setData(demoData);
-    };
-
-    return (
-      <>
-        <button onClick={initData}>Initialize The Experience</button>
-      </>
-    );
-  }
-
-  const displayData = (item) => {
-    return <div>{JSON.stringify(item)}</div>;
+    let demoData = {};
+    try {
+      const rset = await fetch('/api/data');
+      demoData = await rset.json();
+    } catch (err) {
+      console.log('data not found');
+      console.log(err);
+    }
+    localStorage.setItem(key, JSON.stringify(demoData));
+    props.setData(demoData);
   };
 
   return (
     <>
-      <ul>{_.get(data, 'payload', []).map(displayData)}</ul>
+      <button onClick={initData}>Initialize The Experience</button>
     </>
   );
 }
+
+const TableData = (subscriptions) => {
+  const displayData = (item) => {
+    return (
+      <tr key={item.id}>
+        <td>{item.name}</td>
+        <td>{item.price}</td>
+        <td>{item.frequency}</td>
+        <td>edit</td>
+        <td>delete</td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className={styles.home}>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>price</th>
+            <th>frequency</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{_.get(subscriptions, 'payload', []).map(displayData)}</tbody>
+      </table>
+    </div>
+  );
+};
